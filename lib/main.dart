ae918813e1f98f9608c39dda7b291f5d9c0a87b9
Md Rakibul Islam <38698778+Rakibul25@users.noreply.github.com>
2021-01-31 11:51:41 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MaterialApp(
+      title: "Unit Converter",
+      home: UnitConverter(),
+    ));
+
+class UnitConverter extends StatefulWidget {
+  @override
+  _UnitConverterState createState() => _UnitConverterState();
+}
+
+class _UnitConverterState extends State<UnitConverter> {
+  final textcontroller = TextEditingController();
+  double a = 0.00;
+  String b = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Unit Converter"),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height / 1.5,
+        width: MediaQuery.of(context).size.width,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Card(
+            elevation: 3.0,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ListTile(
+                  title: TextField(
+                    onChanged: (text){
+                      b = text;
+                    },
+                    controller: textcontroller,
+                    style: TextStyle(fontSize: 20.0, color: Colors.black),
+                    keyboardType:
+                        TextInputType.numberWithOptions(decimal: true),
+                  ),
+                  trailing: Icon(Icons.add_road),
+                ),
+                IconButton(
+                  icon: Icon(Icons.arrow_downward),
+                  onPressed: () {
+                    setState(() {
+                      a = a+15.00;
+                    });
+                  },
+                ),
+                Text('$b')
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
